0ac15db30de5a77d77e6c449de9b902fbbc71d3c
iledarn <iledarn@gmail.com>
2022-10-10 01:50:47 +0800

    add - return book detail

--- djforms/books/views.py
+++ djforms/books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from .forms import BookFormSet, BookForm
 from .models import Author, Book
 
@@ -14,7 +15,7 @@ def create_book(request, pk):
             book = form.save(commit=False)
             book.author = author
             book.save()
-            return HttpResponse("success")
+            return redirect("detail-book", pk=book.id)
         else:
             return render(request, "partials/book_form.html", context={
                 "form": form
@@ -33,3 +34,11 @@ def create_book_form(request):
     form = BookForm()
     context = {"form": form}
     return render(request, "partials/book_form.html", context)
+
+
+def detail_book(request, pk):
+    book = get_object_or_404(Book, id=pk)
+    context = {
+        "book": book
+    }
+    return render(request, "partials/book_detail.html", context)
--- djforms/djforms/urls.py
+++ djforms/djforms/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from books.views import create_book, create_book_form
+from books.views import create_book, create_book_form, detail_book
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("<pk>/", create_book, name="create-book"),
     path("htmx/create-book-form/", create_book_form, name="create-book-form"),
+    path("htmx/book/<pk>/", detail_book, name="detail-book"),
 ]
