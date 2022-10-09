46cdeefbe8dfce07ada714a640688be0b96fb385
iledarn <iledarn@gmail.com>
2022-10-10 00:11:00 +0800

    add forms, views

--- djforms/books/views.py
+++ djforms/books/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import BookFormSet
+from .models import Author, Book
 
-# Create your views here.
+
+def create_book(request, pk):
+    author = Author.objects.get(id=pk)
+    books = Book.objects.filter(author=author)
+    formset = BookFormSet(request.POST or None)
+
+    if request.method == "POST":
+        if formset.is_valid():
+            formset.instance = author
+            formset.save()
+            return redirect("create-book", pk=author.id)
+
+    context = {"formset": formset, "author": author, "books": books}
+
+    return render(request, "create_book.html", context)
