825e264e8ddc49decf3bce7edd5176b80852eddb
iledarn <iledarn@gmail.com>
2022-10-10 02:49:45 +0800

    add - crispy

--- djforms/djforms/settings.py
+++ djforms/djforms/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'books',
+    'crispy_forms',
+    'crispy_tailwind',
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
