946930de8139ad6fa65995e1450c3bdfe91604b7
iledarn <iledarn@gmail.com>
2022-10-10 02:07:18 +0800

    add - update book

--- djforms/books/views.py
+++ djforms/books/views.py
@@ -17,15 +17,9 @@ def create_book(request, pk):
             book.save()
             return redirect("detail-book", pk=book.id)
         else:
-            return render(request, "partials/book_form.html", context={
-                "form": form
-            })
+            return render(request, "partials/book_form.html", context={"form": form})
 
-    context = {
-        "form": form,
-        "author": author,
-        "books": books
-    }
+    context = {"form": form, "author": author, "books": books}
 
     return render(request, "create_book.html", context)
 
@@ -38,7 +32,19 @@ def create_book_form(request):
 
 def detail_book(request, pk):
     book = get_object_or_404(Book, id=pk)
-    context = {
-        "book": book
-    }
+    context = {"book": book}
     return render(request, "partials/book_detail.html", context)
+
+
+def update_book(request, pk):
+    book = Book.objects.get(id=pk)
+    form = BookForm(request.POST or None, instance=book)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect("detail-book", pk=book.id)
+
+    context = {"form": form, "book": book}
+
+    return render(request, "partials/book_form.html", context)
--- djforms/djforms/urls.py
+++ djforms/djforms/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from books.views import create_book, create_book_form, detail_book
+from books.views import create_book, create_book_form, detail_book, update_book
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path("<pk>/", create_book, name="create-book"),
     path("htmx/create-book-form/", create_book_form, name="create-book-form"),
     path("htmx/book/<pk>/", detail_book, name="detail-book"),
+    path("htmx/book/<pk>/update/", update_book, name="update-book"),
 ]
